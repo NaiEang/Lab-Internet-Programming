1bb699a62503b18b48651e2d5d8f6c7da5950040
Nai Eang <naieang016@gmail.com>
2025-12-30 08:48:44 +0700

    practice 2

---
+++ api-gateway/src/orders/orders.service.ts
@@ -0,0 +1,15 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Injectable()
+export class OrdersService {
+  constructor(@Inject('ORDERS_SERVICE') private readonly client: ClientProxy) {}
+
+  createOrder(orderDto: any) {
+    // In real life we might validate or save to DB first
+    // Here we just emit an event
+    this.client.emit('order_created', orderDto);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    return { status: 'Order accepted', order: orderDto };
+  }
+}
